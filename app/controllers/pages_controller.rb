151ba3a0000329cd463b8f870ead3108876b3745
taufikhidayat1993 <38577145+taufikhidayat1993@users.noreply.github.com>
2024-02-18 01:24:07 +0700

    update tictoctoe finish skenario

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,20 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, except: %i[home hiscores invincible twoplayers]
-  before_action :set_stats, only: %i[game]
+  before_action :authenticate_user!, except: %i[home hiscores invincible twoplayers games play registerplayers result]
+  before_action :initialize_plays, only: [:play]
+  skip_before_action :verify_authenticity_token, only: [:twoplayers, :play, :registerplayers, :reset_board, :result]
+
+  def initialize_plays
+    session[:plays] ||= 0
+  end
+
   def home
   end
 
-  def game
+  def games
   end
 
   def hiscores
-    @users = User.order('win DESC')
+    @users = User.order(win: :desc)
   end
 
   def played
@@ -18,12 +24,141 @@ class PagesController < ApplicationController
     current_user.save
   end
 
-  def invincible 
+  def invincible
+  end
+  
+  def play
+    cell = params[:cell]
+    if session["cell_#{cell}"].present?
+      render json: { success: false, message: '#{cell.inspect}' }
+      return
+    end
+
+    session["cell_#{cell}"] = get_turn
+    add_plays_count
+    win = player_play_win(cell)
+  
+    if !win
+      switch_turn
+     
+      redirect_to twoplayers_path
+    else
+      
+      mark_win(get_turn)
+      reset_board
+      redirect_to result_path
+    end
   end
 
-  def twoplayers
+  def registerplayers
+    session[:player_x_name] = params[:player1_name]
+    session[:player_o_name] = params[:player2_name]
+    set_turn('x')
+    reset_board
+    reset_wins
+    redirect_to twoplayers_path
   end
+  
+  def twoplayers
+     session[:player_x_wins] = params[:player1_name]
+    session[:player_o_wins] = params[:player2_name]
+  
+  end
+
+  def current_player
+    session[:current_player] = get_player_name(get_turn)
+  end
+
   private
+
+  def reset_board
+    reset_plays_count
+    (1..9).each { |i| session.delete("cell_#{i}") }
+  end
+
+  def reset_wins
+    session[:player_x_wins] = 0
+    session[:player_o_wins] = 0
+  end
+
+  def reset_plays_count
+    session[:plays] = 0
+  end
+
+  def plays_count
+    session[:plays].to_i
+  end
+
+  def get_turn
+    session[:turn] || 'x'
+  end
+
+  def set_turn(turn = 'x')
+    session[:turn] = turn
+  end
+
+  def add_plays_count
+    session[:plays] += 1
+  end
+
+  def player_play_win(cell = 1)
+    return false if session[:plays] < 3
+  
+    column = cell % 3
+    column = 3 if column.nil?
+    row = (cell.to_f / 3.0).ceil
+  
+    player = get_turn
+  
+  
+    is_vertical_win(column, player) || is_horizontal_win(row, player) || is_diagonal_win(player)
+  end
+
+  def mark_win(player = 'x')
+    session["player_#{player}_wins"] ||= 0
+    session["player_#{player}_wins"] += 1
+  end
+
+  def switch_turn
+    set_turn(get_turn == 'x' ? 'o' : 'x')
+  end
+
+  def is_vertical_win(column = 1, turn = 'x')
+    column = column.to_i
+  newcell=1
+  get_cell(newcell) == turn &&
+    get_cell(newcell + 3) == turn &&
+    get_cell(newcell + 6) == turn ||   get_cell(newcell+1) == turn &&
+    get_cell(newcell + 4) == turn &&
+    get_cell(newcell + 7) == turn ||   get_cell(newcell+2) == turn &&
+    get_cell(newcell + 5) == turn &&
+    get_cell(newcell + 8) == turn 
+    
+  end
+  
+  def is_horizontal_win(row = 1, turn = 'x')
+    get_cell(row) == turn &&
+      get_cell(row + 1) == turn &&
+      get_cell(row + 2) == turn
+  end
+  
+  def is_diagonal_win(turn = 'x')
+    win = get_cell(1) == turn && get_cell(9) == turn
+
+    unless win
+      win = get_cell(3) == turn && get_cell(7) == turn
+    end
+  
+    win && get_cell(5) == turn
+  end
+
+  def get_cell(cell = '')
+    session["cell_#{cell}"]
+  end
+
+  def score(player = 'x')
+    session["player_#{player}_wins"].presence || 0
+  end
 
   def set_stats
     @wins = current_user.win
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
-    
-    get '/twoplayers', to: 'pages#twoplayers'
+  get '/games', to: 'pages#games'
+  post '/twoplayers', to: 'pages#twoplayers'
+  get '/twoplayers', to: 'pages#twoplayers'
+  post 'registerplayers', to: 'pages#registerplayers'
+  post '/play', to: 'pages#play'
+  get '/result', to: 'pages#result'
+
 
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
